a1f134671c3821eeb469e09f2f9a04ccb8c7338e
Andrea Stacchiotti <andreastacchiotti@gmail.com>
2018-08-27 00:11:44 +0200

    Undo previous patch

--- SpacechemPatch/Patches/AbstractDraggable.cs
+++ SpacechemPatch/Patches/AbstractDraggable.cs
@@ -17,7 +17,7 @@ namespace SpacechemPatch.Patches
         [Decoy("#=q8SkGy0Us0c0Tm2sfolvmMw==")]
         public PipeList<OutputPipe> outputPipes;
         [Injected]
-        private int priority;
+        internal int priority;
 
         [Decoy("#=q_rKTlntlJmkIAnzj3v8jwWocRPnTP6SCM$DxwK7qjyw=")]
         public virtual void MakeTooltip()
@@ -25,42 +25,9 @@ namespace SpacechemPatch.Patches
 
         }
 
-        [Replaced("#=qtrBzOdYZZ65GB65VDAbBsQ==", Patch.ShowReactorPriority, KeepOriginal = true, NewNameForOriginal = "OriginalRender")]
+        [Decoy("#=qtrBzOdYZZ65GB65VDAbBsQ==")]
         public virtual void Render(SpriteBatch spriteBatch, Color color, Vector2i position)
         {
-            if (inputPipes.Count > 0 || outputPipes.Count > 0)
-            {
-                int newPriority = 1;
-                bool found = false;
-                foreach (KeyValuePair<AbstractDraggable, Vector2i> pair in ownerContainer)
-                {
-                    AbstractDraggable draggable = pair.Key;
-                    if (draggable == this)
-                    {
-                        found = true;
-                        break;
-                    }
-                    else if (draggable.inputPipes.Count > 0 || draggable.outputPipes.Count > 0)
-                    {
-                        newPriority++;
-                    }
-                }
-                if (found && newPriority != priority)
-                {
-                    // Building a tooltip is expensive, so only rebuild it if the priority changes.
-                    priority = newPriority;
-                    string nameBackup = name;
-                    name = String.Format("{0} ({1})", name, newPriority);
-                    MakeTooltip();
-                    name = nameBackup;
-                }
-            }
-            OriginalRender(spriteBatch, color, position);
-        }
-
-        public virtual void OriginalRender(SpriteBatch spriteBatch, Color color, Vector2i position)
-        {
-
         }
     }
 }
