75e9ef0fb3b4bfd18f35a91434d030cf0067593b
Csaba Varga <csaboka@gmail.com>
2018-11-17 19:32:50 +0100

    Adding a decoy for RightClickMenu
    This was what I tested the type injection with, so I'm committing it to avoid duplicate work in the future.

---
+++ SpacechemPatch/Patches/RightClickMenu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpacechemPatch.Patches
+{
+    [Decoy("#=qkT0tBhXp89DGtszojsdeygN$RoLg7fOF$VWNqDzaPu0=")]
+    internal class RightClickMenu<T>
+    {
+        [Decoy(".ctor")]
+        public RightClickMenu(Optional<string> title)
+        {
+
+        }
+
+        [Decoy("#=q4jNqleUohYOS6cAKrOZDhg==")]
+        public virtual void Show(IEnumerable<T> selectedElements, Vector2i position)
+        {
+
+        }
+    }
+}
